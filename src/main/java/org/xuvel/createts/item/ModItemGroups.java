6ee37ffff026539f9c8aed21c4f1e58e316fcd43
Xuvel <aleks161208@gmail.com>
2024-08-07 17:48:41 +0300

    Update to 1.2. Added bronze sheets and recipes

--- src/main/java/org/xuvel/createts/item/ModItemGroups.java
+++ src/main/java/org/xuvel/createts/item/ModItemGroups.java
@@ -15,10 +15,11 @@ public class ModItemGroups {
                 .icon(() -> new ItemStack(ModItems.TIN_INGOT))
                 .displayName(Text.translatable("Create: Tempered Steel"))
                 .entries((context, entries) -> {
-                    entries.add(ModItems.TIN_INGOT);
                     entries.add(ModItems.RAW_TIN);
+                    entries.add(ModItems.TIN_INGOT);
                     entries.add(ModItems.TIN_NUGGET);
                     entries.add(ModItems.BRONZE_INGOT);
+                    entries.add(ModItems.BRONZE_SHEET);
                     entries.add(ModItems.BRONZE_NUGGET);
                     entries.add(ModItems.STEEL_INGOT);
                     entries.add(ModItems.STEEL_NUGGET);
@@ -28,6 +29,8 @@ public class ModItemGroups {
                     entries.add(ModBlocks.STEEL_BLOCK);
                     entries.add(ModBlocks.TIN_ORE);
                     entries.add(ModBlocks.DEEPSLATE_TIN_ORE);
+//                    entries.add(ModItems.STEEL_KNIFE);
+//                    entries.add(ModItems.BRONZE_KNIFE);
                 })
                 .build());
     public static void register() {
--- src/main/java/org/xuvel/createts/item/ModItems.java
+++ src/main/java/org/xuvel/createts/item/ModItems.java
@@ -17,9 +17,12 @@ public class ModItems {
     // Bronze
     public static final Item BRONZE_INGOT = registerItem("bronze_ingot", new Item(new FabricItemSettings()));
     public static final Item BRONZE_NUGGET = registerItem("bronze_nugget", new Item(new FabricItemSettings()));
+    public static final Item BRONZE_SHEET = registerItem("bronze_sheet", new Item(new FabricItemSettings()));
+    public static final Item BRONZE_KNIFE = registerItem("bronze_knife", new Item(new FabricItemSettings()));
     // Steel
     public static final Item STEEL_INGOT = registerItem("steel_ingot", new Item(new FabricItemSettings()));
     public static final Item STEEL_NUGGET = registerItem("steel_nugget", new Item(new FabricItemSettings()));
+    public static final Item STEEL_KNIFE = registerItem("steel_knife", new Item(new FabricItemSettings()));
 
     public static Item registerItem(String id, Item item) { return Registry.register(Registries.ITEM, new Identifier(CreateTS.MODID, id), item);}
     public static Item registerItem(String id, Item item, RegistryKey<ItemGroup> itemGroup) {
